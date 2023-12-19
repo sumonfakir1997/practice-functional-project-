eaca9070b2415cb7ef11c0f8fa292607942f267f
Md. Sumon Fakir <sumonfakir1997@gmail.com>
2023-12-20 01:36:21 +0600

    folder stucture update

---
+++ src/pages/basic_functional_operation/string/index.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { Box } from "@chakra-ui/react";
+
+const string = () => {
+
+    const name = "Sumon";
+    const lastName = "Fakir"
+
+    console.log(`my name is ${name} and my surname is ${lastName}`)
+
+
+
+
+    return (
+        <>
+            <Box>
+                string
+            </Box>
+        </>
+    )
+}
+export default string ;
